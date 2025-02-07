4fbea8838707b2eece092538a814549027a1db6c
ErosJanka <erosjanka@gmail.com>
2025-02-07 14:17:44 -0300

    TRY - CATCH - FINNALY

---
+++ aula45/try.catch.finally.js
@@ -0,0 +1,17 @@
+//                                                  TRY - CATCH - FINNALY
+
+/*
+try { //executado quando não há erros
+    console.log('abri um arquivo');
+    console.log('Manipulei o arquivo e gerou erro'); //por esse erro o try encerra e manda para o CATCH
+    console.log('fechei o arquivo'); //por conta do erro não chegaria nessa linha de codigo
+}catch(erro){ //executado quando há erro
+    console.log('tratando o erro'); //não fecharia o arquivo aqui
+} finally{ //executado sempre. Mas pode omitir se não precisar que o codigo seja executado 
+    console.log('FINNALY: Eu sempre sou executado') //pode sempre tentar fechar no finally ao invés no try.
+}
+*/
+
+function retornaHora(data){//quero que 'data' seja exatamente um objeto de Date
+
+}
