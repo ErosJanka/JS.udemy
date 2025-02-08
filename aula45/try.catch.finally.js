9d3ecb0f2dfc28c69ad0368efc44bf785d449ba5
ErosJanka <156869410+ErosJanka@users.noreply.github.com>
2025-02-08 09:45:38 -0300

    fanally

--- aula45/try.catch.finally.js
+++ aula45/try.catch.finally.js
@@ -13,5 +13,29 @@ try { //executado quando não há erros
 */
 
 function retornaHora(data){//quero que 'data' seja exatamente um objeto de Date
+    if(data && !(data instanceof Date)){ //Se a data for lançada e não for uma instancia de Date...
+        throw new TypeError('Eperando instancia de Date'); //Estou motrando qual é o erro que daria caso não coloque uma data.
+    }
 
+    if(!data){ //Se a data não for enviada
+        data = new Date()
+    }
+
+    return data.toLocaleTimeString('pr-BR', {
+        hour: '2-digit', //garantindo que a hora apareça com 2 digitos ex: 09:00 ao invés de 9:00
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false //para não aparecer o formado de 12 horas ex: 9:34 Pm
+    });
+}
+
+try{
+const data = new Date('01-01-1970 09:34:12');
+const hora = retornaHora();
+console.log(hora)
+} catch(erro){
+    //tratar erro
+    console.log(erro)
+} finally{
+    console.log('Tenha um bom dia')
 }
