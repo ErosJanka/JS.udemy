e1da1af38eb16baf6163f78a7ad0ea30f407d01b
ErosJanka <156869410+ErosJanka@users.noreply.github.com>
2025-02-20 15:43:21 -0300

    aprimorando

--- aula47/professor/assets/js/main.js
+++ aula47/professor/assets/js/main.js
@@ -23,14 +23,46 @@ console.log(criaHoraDosSegundos(10));
 
 
 iniciar.addEventListener('click', function(event){
+    relogio.classList.remove('pausado'); //quando iniciar ele remove o vermelho
+    clearInterval(timer); //para que ele não fique criando varios contadores e o contador não fique acelerando
     iniciaRelogio()
 }); 
 
 pausar.addEventListener('click', function(event){
     clearInterval(timer); //quando eu clico e ele para, mas volta no mesmo contador, porque a variavel segundos mantém o seu estado. não esta criando ela novamente
+    relogio.classList.add('pausado'); //quando pausar ele fica vermelho
 }); 
 
 zerar.addEventListener('click', function(event){
     clearInterval(timer);
     relogio.innerHTML = '00:00:00';
-}); 
+    segundos = 0;
+});
+
+/*
+Ao invés de de ficar criando varios addEventListener, podemos fazer o seguinte:
+
+document.addEventListener('click', function(event){
+    //console.log(event.target); //vai falar qual elemento está sendo clicado
+
+    const element = event.target;
+
+    if(element.classList.contains('zerar')){
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado'); //quando zerar ele remove o vermelho
+        segundos = 0;
+    }
+
+    if(element.classList.contains('iniciar')){
+        relogio.classList.remove('pausado'); //quando iniciar ele remove o vermelho
+        clearInterval(timer); //para que ele não fique criando varios contadores e o contador não fique acelerando
+        iniciaRelogio()
+    }
+
+    if(element.classList.contains('pausar')){
+        clearInterval(timer); //quando eu clico e ele para, mas volta no mesmo contador, porque a variavel segundos mantém o seu estado. não esta criando ela novamente
+        relogio.classList.add('pausado'); //quando pausar ele fica vermelho
+    }
+});
+*/
