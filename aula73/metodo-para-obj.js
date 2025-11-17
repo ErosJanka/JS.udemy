4173aa088e0036a9962312ddfcf5b1fda3996913
ErosJanka <erosjanka@gmail.com>
2025-11-17 11:59:58 -0300

    metodos-para-objetos

--- aula73/metodo-para-obj.js
+++ aula73/metodo-para-obj.js
@@ -13,45 +13,74 @@ Object.defineProperty (define uma propriedade)
 */
 
 const produto = {
-    nome: 'Caneca',
-    preco: 1.80
+    nome: 'Caneca', preco: 1.8
 }
+
 const outraCoisa = produto
 
+produto.nome = 'Geovanna Linda' //alterndo o objeto nome 
+outraCoisa.preco = 1.5 //Aqui altera também tanto a variavel produto quanto o outraCoisa
 
-outraCoisa.nome = "Geovanna Linda"
-outraCoisa.preco = 3.80
 console.log(produto)
 console.log(outraCoisa)
-console.log(Object.keys(produto+))
 
-//dessa maneira acima tanto o produto quanto a outraCoisa são os mesmos valores,se eu alterar um altera outro, agora para copiar sem alterar as duas variaveis teria que usar o spread Operator. Abaixo mostrarei como:
+//abaxio é para saber como ter a variavel copiada e não apontando para o mesmo locaal como acima
 
-const produto2 = {
-    nome: 'xicara',
-    preco: 2.80
-}
-const outraCoisa2 = {
-    ... produto,
-    material: 'Glitter' //posso adicionar mais  coisas
+
+//utilizando sprad operator
+const original = {
+    nome: 'Geovanna maravilhosa',
+    preco: 'infinito'
 }
 
-
-outraCoisa2.nome = "Geovanna Maravilhosa"
-outraCoisa2.preco = 10.80
-console.log(produto2)
-console.log(outraCoisa2)
-
-//Utilizando assign
-const produto3 = {
-    nome: 'Copo',
-    preco: 0.80
+const copia = { 
+    ...original, 
+    material: 'luz' //posso adicionar mais coisas
 }
-const outraCoisa3 = Object.assign({}, produto3, {material: 'Luz Solar'})
+
+original.nome = 'Geovanna meu amor' //aqui consigo alterar a vaariavel original sem modificar a copia junto
+
+console.log(original)
+console.log(copia)
+
+//utilizando Object.assign
+const original2 = {
+    nome: 'Eros',
+    preco: 1.5
+}
+
+const copia2 = Object.assign({}, original2, {material: 'Madeira Bruta'})
+
+copia2.preco = 2.5
+
+console.log(original2)
+console.log(copia2)
 
 
-outraCoisa3.nome = "Geovanna Espetacular"
-outraCoisa3.preco = 100.80
-console.log(produto3)
-console.log(outraCoisa3)
+////
 
+console.log(Object.keys(original2))
+Object.freeze(original2) //Aqui não me permite mudar nada desse objeto 'original2'
+
+
+///// mudando propriedades dos objetos
+Object.defineProperty(original, 'nome', {
+    writable: false, //não pode mais alterar essaa propriedade
+    configurable: false,
+    value: 'outra coisa'
+})
+console.log(Object.getOwnPropertyDescriptor(original, 'nome'))
+original.nome = 'Geovanna mudada' //não consigo alterar 
+delete original.nome; //também não consigo excluir
+delete original.preco; //aqui consigo excluir
+console.log(original)
+
+//mostrando os valores e não a chave:
+console.log(Object.values(produto))
+
+//se eu quiser ler os dois em um array:
+console.log(Object.entries(produto)) // [ [ 'nome', 'Geovanna Linda' ], [ 'preco', 1.5 ] ]
+//com isso posso iterar sobre esses valores:
+for(let entry of Object.entries(produto)){
+    console.log(entry)
+}
